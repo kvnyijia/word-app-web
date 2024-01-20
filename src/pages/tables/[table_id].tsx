294a91d435ba1a494a8ecb930d31af9bc3d03277
Yijia Chen <kevinc110604@gmail.com>
2024-01-20 11:14:14 -0500

    [new & refactor] Add toast when deleting words

--- src/pages/tables/[table_id].tsx
+++ src/pages/tables/[table_id].tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIcon, DeleteIcon } from "@chakra-ui/icons";
-import { Box, Flex, IconButton, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Image, SimpleGrid, Text, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { CreateWordPopover } from "../../components/CreateWordPopover";
@@ -9,14 +9,41 @@ import { wordServices } from "../../utils/wordServices";
 const Table = () => {
   const router = useRouter();
   const table_id = router.asPath.substring(8);
+  const toast = useToast();
 
   const [words, setWords] = useState<any[]>([]);
-  useEffect(() => {
+  function getWords(table_id: number | string) {
     wordServices.getWords(table_id)
       .then(({ resJson }) => {
         setWords(resJson.words);
       })
       .catch(() => {});
+  }
+
+  function deleteWord(table_id: number | string, word_id: number) {
+    wordServices.deleteWord(word_id)
+      .then(() => {
+        getWords(table_id);
+        toast({
+          title: 'Word deleted.',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: 'Errors happened, try again.',
+          description: JSON.stringify(err),
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+  }
+
+  useEffect(() => {
+    getWords(table_id);
   }, [table_id]);
 
   const body = !words ? (
@@ -48,11 +75,7 @@ const Table = () => {
             isRound={true}
             style={{border: "solid"}}
             onClick={() => {
-              wordServices.deleteWord(word.id)
-                .then(() => {
-                  window.location.reload();
-                })
-                .catch(() => {});
+              deleteWord(table_id, word.id);
             }}
           />
         </Box>
