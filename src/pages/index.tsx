51794dbea91f6c5e492d2b45214aa5ab0d09d955
Yijia Chen <kevinc110604@gmail.com>
2024-01-20 12:57:52 -0500

    [new & refactor] Add deleting table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { NavBar } from '../components/NavBar';
 import { Layout } from '../components/Layout';
-import { Button, Flex, SimpleGrid, Text } from '@chakra-ui/react';
+import { Button, Flex, HStack, IconButton, SimpleGrid, Text, useToast } from '@chakra-ui/react';
 import NextLink from "next/link";
 import { CreateTablePopover } from '../components/CreateTablePopover';
 import { useState, useEffect } from 'react';
 import { userServices } from '../utils/userServices';
 import { tableServices } from '../utils/tableServices';
+import { DeleteIcon } from '@chakra-ui/icons';
 
 interface loginUserRecord {
   username: string;
@@ -13,6 +13,7 @@ interface loginUserRecord {
 }
 
 const Index = () => {
+  const toast = useToast();
 
   const [loginUser, setLoginUser] = useState<loginUserRecord>({} as loginUserRecord);
   useEffect(() => {
@@ -38,6 +39,10 @@ const Index = () => {
 
   const [tables, setTables] = useState<any[]>([]);
   useEffect(() => {
+    getTables(loginUser, authTokenIsValid);
+  }, [loginUser, authTokenIsValid]);
+
+  function getTables(loginUser, authTokenIsValid) {
     if (loginUser && authTokenIsValid) {
       tableServices.getTables(username)
         .then(({ resJson }) => {
@@ -45,16 +50,52 @@ const Index = () => {
         })
         .catch(() => {});
     }
-  }, [loginUser, authTokenIsValid]);
+  }
+
+  function deleteTable(table_id) {
+    tableServices.deleteTable(table_id)
+      .then(() => {
+        getTables(loginUser, authTokenIsValid);
+        toast({
+          title: 'Table deleted.',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: 'Errors happened, try again.',
+          description: JSON.stringify(err),
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        });
+      });
+  }
 
   const buildTables = () => {
     return (
       <SimpleGrid columns={2} spacing={4}>
         {tables.map((table, index) => {
           return (
-            <NextLink key={index} href={`tables/${table.id}`}>
-              <Button key={index} padding={10} w={400}>{table.name}</Button>
-            </NextLink>
+            <HStack>
+              <NextLink key={index} href={`tables/${table.id}`}>
+                <Button key={index} padding={10} w={400}>{table.name}</Button>
+              </NextLink>
+              <IconButton
+                aria-label="Delete"
+                icon={<DeleteIcon/>}
+                size='xs'
+                mt={3}
+                p={3}
+                variant="link"
+                colorScheme="red"
+                isRound={true}
+                style={{border: "solid"}}
+                onClick={() => { deleteTable(table.id); }}
+              />
+            </HStack>
           );
         })}
       </SimpleGrid>
--- src/utils/tableServices.ts
+++ src/utils/tableServices.ts
@@ -5,6 +5,7 @@ export const tableServices = {
   createTable,
   getTables,
   getTable,
+  deleteTable,
 };
 
 function createTable(jsonData) {
@@ -23,6 +24,13 @@ function getTables(username) {
 
 function getTable(table_id) {
   return fetchWrapper.get(`${apiUrl}/tables/${table_id}`)
+    .then(async ({resJson, ok}) => {
+      return {resJson, ok};
+    });
+}
+
+function deleteTable(table_id) {
+  return fetchWrapper._delete(`${apiUrl}/tables/${table_id}`)
     .then(async ({resJson, ok}) => {
       return {resJson, ok};
     });
